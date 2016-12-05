37b32ac7be7b7e91f36c56a8fabf9c95f99186a8
Jaime Díaz García <jimmydg94@gmail.com>
2016-12-05 14:13:07 +0100

    Anotaciones Para combate

--- app/src/main/java/com/grupo/the_end_is_near/escenario/Combate.java
+++ app/src/main/java/com/grupo/the_end_is_near/escenario/Combate.java
@@ -2,4 +2,13 @@ package com.grupo.the_end_is_near.escenario;
 
 
 public class Combate {
+    //TODO
+    //1. Colocar personajes con la animación estandar (parados)
+    //2. Animaciones para el ataque de Heroes y de enemigos
+            //El heroe que ataque se mueve un poco a la izquierda y si es un enemigo hacia la derecha.
+            //A continuación vuelven a su posición inicial
+    //3. Animación para el Heroe recibiendo el daño. El enemigo lo mismo.
+    //4. Animación de muerte para cada Heroe y enemigo (este podría simplemente desaparecer)
+    //5. Pantalla de victoria/derrota
+
 }
