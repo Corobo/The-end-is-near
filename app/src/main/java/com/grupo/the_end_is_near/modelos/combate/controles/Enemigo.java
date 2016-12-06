b8a51397b6b2394503276acd47dada9093bb01d4
DESKTOP-SOVD54J\Corobo <adrianjv@hotmail.es>
2016-12-06 23:45:44 +0100

    Coses

---
+++ app/src/main/java/com/grupo/the_end_is_near/modelos/combate/controles/Enemigo.java
@@ -0,0 +1,10 @@
+package com.grupo.the_end_is_near.modelos.combate.controles;
+
+import com.grupo.the_end_is_near.modelos.Modelo;
+
+/**
+ * Created by Corobo on 06/12/2016.
+ */
+
+public class Enemigo extends Modelo {
+}
